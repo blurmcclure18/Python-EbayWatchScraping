b4d80099f68d487ab72c16b94ec63e87f7978913
blurmcclure18 <42517722+blurmcclure18@users.noreply.github.com>
2022-01-01 21:36:57 -0600

    updated captcha

--- ebaycrawl/selenium_ebay.py
+++ ebaycrawl/selenium_ebay.py
@@ -1,11 +1,11 @@
-import selenium
-import undetected_chromedriver as uc
+from selenium import webdriver as wd
 
+fp = wd.FirefoxProfile('/home/alec/.mozilla/firefox/87syjf8o.default-release')
+wd = wd.Firefox(fp)
 
-driver =uc.Chrome()
+wd.implicitly_wait(10)
 
 url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=grade+303+elgin&LH_Complete=1&LH_Sold=1"
 
-driver = uc.Chrome()
-
-driver.get(url)
+wd.get(url)
+pagesource = wd.page_source
