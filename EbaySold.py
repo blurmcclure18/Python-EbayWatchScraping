1627f0e1f9faf97e0028df15848041fddf64a681
blurmcclure18 <42517722+blurmcclure18@users.noreply.github.com>
2022-01-03 23:50:02 -0600

    added comments to EbaySold

--- EbaySold.py
+++ EbaySold.py
@@ -11,35 +11,39 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
+# Get Current Working Directory to use in Functions
 currentDir = os.path.dirname(__file__)
 SourceFiles = "SourceFiles"
-
 SourceFilesDir = currentDir + '/' + SourceFiles
 
+# Create funtions to use in program
 def createSourceDir():
+    # Create the folder to store temp html files
     try:
         os.mkdir(SourceFiles)
     except:
         pass
 
 def fool():
+    # Using random sleep duration to help fool captcha
     sleep(randint(1,10))
 
 def setupBrowser():
-    
-    #myFirefoxProfile = Path(r'/home/alec/Documents/GitHub/Python-EbayAPI/FirefoxProfile/ebayProfile')
+    # Create and launch a FireFox Browser 
+
+    # Firefox Proile Location
     firefoxProfile = Path(rf"{currentDir}/FirefoxProfile/EbayProfile/")
 
-    # use firefox profile
+    # Use Firefox profile
     fp = wd.FirefoxProfile(firefoxProfile)
 
-    # Ebay URL
+    # Ebay Sold Listings URL
     ebay_soldUrl = "https://www.ebay.com/sch/i.html?_odkw=&_ipg=25&_sadis=200&_adv=1&_sop=12&LH_SALE_CURRENCY=0&LH_Sold=1&_osacat=0&_from=R40&_dmd=1&LH_Complete=1&_trksid=m570.l1313&_nkw=replacethisword&_sacat=0"
 
-    # create and start firefox browser
+    # Store options to use in Firefox
     firefoxOptions = Options()
 
-    # start a headless browser (comment out the below line to view what the browser is doing )
+    # Start a headless browser (comment out the below line to view what the browser is doing )
     firefoxOptions.headless = True
 
     # Run the browser
@@ -47,9 +51,11 @@ def setupBrowser():
     browser.implicitly_wait(10)
     browser.get(ebay_soldUrl)
 
+    # Return the browser to use in Threading
     return browser
 
 def perform_actions(watchGrade, browser):
+    # Automate the browser using selenium
 
     # input ebay Search
     search_xpath = '//*[@id="gh-ac"]'
@@ -75,34 +81,43 @@ def perform_actions(watchGrade, browser):
     )
     items_200.click()
 
-    # perform action on Ebay tab
+    # Get the page source html
     ebay_pagesource = browser.page_source
     
     # Close the browser
     browser.quit()
 
+    # Parse html into Soup with BeautifulSoup
     soup = BeautifulSoup(ebay_pagesource, "html.parser")
 
+    # Remove "Related Items" section that would give false info
     newSoup = soup
     soup.find("div", {'class': 'srp-merch__items clearfix'}).decompose()
     
+    # Write the Beautified Soup to html file for parsing
     with open(os.path.join(SourceFilesDir, watchGrade + ".html"),'w') as writer:
         writer.write(str(newSoup))
 
 def parse_sold(watchGrade):
+    # Parsing html to get sold prices
+
+    # Create lock for threading
     global lock
     lock = threading.Lock()
 
+    # Get the counter to create dictionary entries
     global resultsCounter
 
+    # Import and rebeautify it
     soup = open(SourceFilesDir + '/' + f'{watchGrade}.html')
     newSoup = BeautifulSoup(soup, 'html.parser')
+    
+    # Get all Sold Listings Data
     results = newSoup.find_all('div', {'class': 's-item__info clearfix'})
 
-    numResults = len(results)
-
+    # With Threading iterate adding sold prices to list and average them
     with lock:
-        
+        # list to store sold prices
         gradeSoldPrices = []
         
         for item in results:
@@ -115,41 +130,57 @@ def parse_sold(watchGrade):
         
         averagePrice = round(sum(gradeSoldPrices) / len(gradeSoldPrices))
         
-        myResults[resultsCounter] = {'grade': watchGrade, 'averagePrice':averagePrice}
+        # Add the watch grade and average price to dictionary 
+        watchResults[resultsCounter] = {'grade': watchGrade, 'averagePrice':averagePrice}
+
+        # Increase counter for next iteration
         resultsCounter += 1
 
 def get_handles(watchGrade, browser):
-    
+    # Using threading perform these funtions
+
     perform_actions(watchGrade, browser)
 
     parse_sold(watchGrade)
 
 def setup_workers(grade_list):
-    
+    # create workers and list to perform threading
     workers = len(grade_list)
     browsers = []
-    counter = 0
     
+    # Counter for while loop
+    counter = 0
+
+    # Add open browsers to list to use
     while len(browsers) < workers:
         browsers.append(setupBrowser())
         counter += 1
 
+    # Using ThreadPool execute our funtions
     with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
         executor.map(get_handles, grade_list, browsers)
 
+# Create Main Function
 def main(gradeList):
-
+    # Run our Python Program
     createSourceDir()
 
     setup_workers(gradeList)
     
-    print(myResults)
+    # Print our watch grades with their average prices
+    print(watchResults)
     
+    # Remove our SourceFiles directory to save space
     sh.rmtree(SourceFilesDir, ignore_errors=True)
 
+# Add Keywords for Ebay Search
 watchgradeList = ["291 elgin", "303 elgin"]
 
+# Create a counter to increment results dictionary
 resultsCounter = 0
-myResults = {}
 
+# Results dictionary that will hold our watch grade with their average price
+watchResults = {}
+
+# Call Main Function
 main(watchgradeList)
