cfc4d415c432407103f1bd47055e22a8effd0759
blurmcclure18 <42517722+blurmcclure18@users.noreply.github.com>
2022-01-03 23:30:02 -0600

    removed unneeded files

--- ExampleCode/urlincrement.py
+++
@@ -1,8 +0,0 @@
-url_count = 1
-url = "https://pocketwatchdatabase.com/guide/company/elgin/grade/{}/value"
-
-while url_count <= 50:
-    print(url.format(url_count))
-    url_count += 1
-
-print("Done!")
